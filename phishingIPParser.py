66130b83cfaa6d834cd8076e1e9f409fef50a5a3
hun <chamgf5247@naver.com>
2024-11-21 13:59:11 +0900

    paser 기능 구현, DB연동 추가 필요

--- phishingIPParser.py
+++ phishingIPParser.py
@@ -4,11 +4,13 @@ import sys
 with open(sys.argv[1]) as f:
     jsonData = json.load(f)
 
+ips=set()
 
-    for idx,data in enumerate(jsonData):
-        try:
-            print('num : ' , idx)
-            print('url : ' + data['url'])
-            print(data['details'][0]['ip_address'])
-        except: 
-            print('nothing')
+for idx,data in enumerate(jsonData):
+    url = data['url']
+    details = data['details']
+
+    for detail in details:
+        ips.add(detail['ip_address'])
+
+print(ips)
